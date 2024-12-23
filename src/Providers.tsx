592efdddd967e88ca525380cd29b5f2f14048206
vincentkua <zylyn89@gmail.com>
2024-12-23 22:57:02 +0800

    feat: added react router dom

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,9 +1,30 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const Providers = () => {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <div
+          style={{
+            padding:"4px",
+            display: "flex",
+            alignItems: "center",
+            gap: "6px",
+            height: "30px",
+            backgroundColor: "#d4d4d4",
+          }}
+        >
+          <Link to={"/"}>Home </Link>
+          <Link to={"/about"}>About </Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <>
+      <h5>About Page</h5>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+        repellat delectus, nesciunt ullam iusto odit dicta cum vero quam
+        reiciendis aut nostrum illo velit. Voluptate veritatis vero libero
+        similique modi.
+      </p>
+    </>
+  );
+};
+
+export default About;
